465518629f0c47780ffdd7e33865806ec95f7cf6
lvarezhkal <kkakxaa@gmail.com>
2024-09-16 21:57:58 +0300

    Fix

--- src/main/java/pro/sky/animalShelter/service/ReportPhotoService.java
+++ src/main/java/pro/sky/animalShelter/service/ReportPhotoService.java
@@ -1,6 +1,5 @@
 package pro.sky.animalShelter.service;
 
-
 import com.pengrad.telegrambot.request.SendPhoto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
